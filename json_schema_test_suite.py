7a0372bcf364915feaba2a6d92571c75cfbe0f95
Kris <krismolendyke@users.noreply.github.com>
2018-03-13 09:13:23 -0400

    Refactor

--- json_schema_test_suite.py
+++ json_schema_test_suite.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from collections import defaultdict
+from collections import Counter, defaultdict, namedtuple
 from textwrap import dedent
 import argparse
 import json
 import sys
+
+try:
+    from enum import Enum, auto
+except ImportError:
+    from enum34 import Enum, auto
 
 try:
     from pathlib import Path
@@ -16,6 +22,15 @@ except ImportError:
     from pathlib2 import Path
 
 import fastjsonschema
+
+class TestResult(Enum):
+    FALSE_POSITIVE = auto()
+    TRUE_POSITIVE = auto()
+    FALSE_NEGATIVE = auto()
+    TRUE_NEGATIVE = auto()
+    UNDEFINED = auto()
+
+Test = namedtuple("Test", "description exception result")
 
 
 def _get_parser():
@@ -25,79 +40,83 @@ def _get_parser():
 
 
 def _main():
-    blacklist = [
-        "additionalProperties",
-        "definitions",
-        "dependencies",
-        "patternProperties",
-    ]
     tests_dir_path = Path().resolve() / "JSON-Schema-Test-Suite/tests/draft4"
-    tests = {
-        "files": 0,
-        "cases": 0,
-        "tests": 0
-    }
-    exceptions = defaultdict(dict)
-
+    tests_ = defaultdict(dict)
     test_files_glob = tests_dir_path.glob("*.json")
     for test_file_path in test_files_glob:
-        if test_file_path.stem not in blacklist:
-            with test_file_path.open() as f:
-                tests["files"] += 1
-                test_data = json.loads(f.read())
-                for test_case in test_data:
-                    tests["cases"] += 1
-                    test_case_description = test_case["description"]
-                    schema = test_case["schema"]
+        with test_file_path.open() as f:
+            tests_[test_file_path.name] = defaultdict(dict)
+            test_data = json.loads(f.read())
+            for test_case in test_data:
+                test_case_description = test_case["description"]
+                schema = test_case["schema"]
+                tests_[test_file_path.name][test_case_description] = []
+                try:
+                    validate = fastjsonschema.compile(schema)
+                except Exception as e:
+                    pass  # TODO record undefined schema exceptions
+                for test in test_case["tests"]:
+                    description = test["description"]
+                    data = test["data"]
+                    result = exception = None
                     try:
-                        validate = fastjsonschema.compile(schema)
+                        if test["valid"]:
+                            try:
+                                validate(data)
+                                result = TestResult.TRUE_POSITIVE
+                            except fastjsonschema.exceptions.JsonSchemaException as e:
+                                result = TestResult.FALSE_NEGATIVE
+                                exception = e
+                        else:
+                            try:
+                                validate(data)
+                                result = TestResult.FALSE_POSITIVE
+                            except fastjsonschema.exceptions.JsonSchemaException as e:
+                                result = TestResult.TRUE_NEGATIVE
+                                exception = e
                     except Exception as e:
-                        if "fastjsonschema" not in exceptions[test_file_path.name]:
-                            exceptions[test_file_path.name]["fastjsonschema"] = []
-                        exceptions[test_file_path.name]["fastjsonschema"].append("[{}] {}: {}".format(type(e).__name__, test_case_description, e))
-                    for test in test_case["tests"]:
-                        tests["tests"] += 1
-                        description = test["description"]
-                        data = test["data"]
-                        try:
-                            validate(data)
-                        except fastjsonschema.exceptions.JsonSchemaException as e:  # Validation exception
-                            if test["valid"]:
-                                if test_case_description not in exceptions[test_file_path.name]:
-                                    exceptions[test_file_path.name][test_case_description] = []
-                                exceptions[test_file_path.name][test_case_description].append("[{}] {}: {}".format(type(e).__name__, description, e))
-                        except Exception as e:  # Any other exception (likely Python)
-                            if test_case_description not in exceptions[test_file_path.name]:
-                                exceptions[test_file_path.name][test_case_description] = []
-                            exceptions[test_file_path.name][test_case_description].append("[{}] {}: {}".format(type(e).__name__, description, e))
+                        result = TestResult.UNDEFINED
+                        exception = e
+                    tests_[test_file_path.name][test_case_description].append(Test(description, exception, result))
 
-    print("\nSkipped:\n")
-    for i, name in enumerate(blacklist, 1):
-        print("{}. {}.json".format(i, name))
-    if exceptions:
-        print("\nFailure summary:")
-        file_exceptions = test_case_exceptions = total_exceptions = 0
-        for file_name, test_cases in exceptions.items():
-            file_exceptions += 1
-            print("\n{}. {}".format(file_exceptions, file_name))
-            j = 0
-            for test_case_description, failures in test_cases.items():
-                j += 1
-                test_case_exceptions += 1
-                print("    {}. {}".format(j, test_case_description))
-                for i, failure in enumerate(failures, 1):
-                    total_exceptions += 1
-                    print("        {}. {}".format(i, failure))
-        sys.exit(dedent("""
-        Failures:
-            tests: {}/{} {:.1%}
-            cases: {}/{} {:.1%}
-            files: {}/{} {:.1%}\
-        """).format(
-            total_exceptions, tests["tests"], total_exceptions / tests["tests"],
-            test_case_exceptions, tests["cases"], test_case_exceptions / tests["cases"],
-            file_exceptions, tests["files"], file_exceptions / tests["files"])
-        )
+    file_exceptions = test_case_exceptions = total_exceptions = 0
+    test_results = Counter()
+    for file_name, test_cases in tests_.items():
+        file_exceptions += 1
+        print("\n{}. {}".format(file_exceptions, file_name))
+        j = 0
+        for test_case_description, test_case in test_cases.items():
+            j += 1
+            test_case_exceptions += 1
+            print("    {}. {}".format(j, test_case_description))
+            for i, test in enumerate(test_case, 1):
+                test_results.update({test.result: True})
+                if test.result == TestResult.TRUE_POSITIVE:
+                    print("        {}. ✔ {} {}".format(i, test.result.name, test.description))
+                elif test.result == TestResult.TRUE_NEGATIVE:
+                    print("        {}. ✔ {} {}".format(i, test.result.name, test.description))
+                elif test.result == TestResult.FALSE_POSITIVE:
+                    print("        {}. ✘ {} [{}] {}: {}".format(i, test.result.name, type(test.exception).__name__, test.description, test.exception))
+                elif test.result == TestResult.FALSE_NEGATIVE:
+                    print("        {}. ✘ {} [{}] {}: {}".format(i, test.result.name, type(test.exception).__name__, test.description, test.exception))
+                elif test.result == TestResult.UNDEFINED:
+                    print("        {}. ⚠ {} [{}] {}: {}".format(i, test.result.name, type(test.exception).__name__, test.description, test.exception))
+                else:
+                    print("        {}. 💀 DEATH")  # WTF
+
+    total = sum(test_results.values())
+    total_failures = total_passes = 0
+    print("\nSummary of {} tests:\n".format(total))
+    print("Failures:\n")
+    for result in (TestResult.FALSE_POSITIVE, TestResult.FALSE_NEGATIVE, TestResult.UNDEFINED):
+        total_failures += test_results[result]
+        print("{:<14} {} {:>5.1%}".format(result.name, test_results[result], test_results[result] / total))
+    print("               {} {:>5.1%}".format(total_failures, total_failures / total))
+    print("\nPasses:\n")
+    for result in (TestResult.TRUE_POSITIVE, TestResult.TRUE_NEGATIVE):
+        total_passes += test_results[result]
+        print("{:<13} {} {:5.1%}".format(result.name, test_results[result], test_results[result] / total))
+    print("              {} {:5.1%}".format(total_passes, total_passes / total))
 
 
 if __name__ == "__main__":
